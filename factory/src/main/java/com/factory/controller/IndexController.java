33aef50210c40b387f60336ef9f6401e9ec122d8
Username1899 <211117579@edu.vut.ac.za>
2017-12-07 11:23:27 +0800

    Created three resources and and index controller Removed activitiBpmn and spring-session dependencies Changed @GetMapping to @PostMapping on our index controller Changed our meta tag on index.html

--- factory/src/main/java/com/factory/controller/IndexController.java
+++ factory/src/main/java/com/factory/controller/IndexController.java
@@ -2,11 +2,12 @@ package com.factory.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class IndexController {
 
-    @GetMapping("/")
+    @PostMapping("/")
     private String index(){
         return "index";
     }
