0cbdbe1ccc404d74fbf3fe0c907784926cc15a0c
Username1899 <211117579@edu.vut.ac.za>
2017-12-08 15:33:07 +0800

    Created three resources and and index controller Removed activitiBpmn and spring-session dependencies Changed @GetMapping to @PostMapping on our index controller Changed our meta tag on index.html Changed @PostMapping to @RequestMapping on our index controller Added factory form Added View factories form Added two images lion and factory Added a factories view form or table Added Department and Employee models Added Department and Employee repository

---
+++ factory/src/main/java/com/factory/repository/DepartmentRepository.java
@@ -0,0 +1,9 @@
+package com.factory.repository;
+
+import com.factory.model.Department;
+import com.factory.model.Employee;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface DepartmentRepository extends JpaRepository<Department, Long>{
+    Department findByName(String name);
+}
---
+++ factory/src/main/java/com/factory/repository/EmployeeRepository.java
@@ -0,0 +1,9 @@
+package com.factory.repository;
+
+import com.factory.model.Employee;
+import com.factory.model.Factory;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface EmployeeRepository extends JpaRepository<Employee, Long>{
+    Employee findByName(String name);
+}
